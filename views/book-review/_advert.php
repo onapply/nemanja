b4d0424dc24f380e7d6ea1cd5dfe545de6d6387b
Nemanja Stankovic <superneminator@gmail.com>
2017-08-10 16:25:07 +0200

    Add files via upload
    Even though these files are not related to the Project, I have decided to upload them, so you could see that I am really learning. I know it is a basic stuff, but we all need to start somewhere :)

---
+++ views/book-review/_advert.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!--Tel IE to use the latest rendering engine-->
+        <meta http-equiv="X-UA-Compatibile" content="IE=edge">
+        <!--Set the page width to the size of the device and set the zoom level to 1-->
+        <meta name="viewport" content="width = device-width, initial-scale = 1">
+        <title>Bootstrap Tutorial</title>
+       
+        <!--Bootstrap css-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <style>
+           
+        </style>
+    </head>
+    
+    <body>
+     
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div class="thumbnail">
+                        <img src="img/onapply_1.png">
+                        <div class="Caption">
+                            <h3>Start now</h3>
+                            <p>Click on the button bellow to start your free trial at <?=$company?></p>
+                            <a href="#" class="btn btn-danger btn-lg" role="button">Start</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div class="thumbnail">
+                        <img src="img/onapply_1.png">
+                        <div class="Caption">
+                            <h3>Start now</h3>
+                            <p>Click on the button bellow to start your free trial.</p>
+                            <a href="#" class="btn btn-danger btn-lg" role="button">Start</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div class="thumbnail">
+                        <img src="img/onapply_1.png">
+                        <div class="Caption">
+                            <h3>Start now</h3>
+                            <p>Click on the button bellow to start your free trial.</p>
+                            <a href="#" class="btn btn-danger btn-lg" role="button">Start</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div class="thumbnail">
+                        <img src="img/onapply_1.png">
+                        <div class="Caption">
+                            <h3>Start now</h3>
+                            <p>Click on the button bellow to start your free trial.</p>
+                            <a href="#" class="btn btn-danger btn-lg" role="button">Start</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!--Bootstrap js-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
